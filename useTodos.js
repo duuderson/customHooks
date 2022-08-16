deaf8eee72576abb536ee1b5a0700b221f278273
Duderson <uberomegabrooficial@gmail.com>
2022-08-16 15:23:05 -0700

    First commit

---
+++ useTodos.js
@@ -0,0 +1,47 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState=[];
+
+const init = () => {
+  return JSON.parse( localStorage.getItem("todos")) || [];
+}
+
+export const useTodos = () => {
+  
+  const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init );
+    
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: '[TODO] Add Todo',
+      payload: todo
+    }
+    dispatchTodo(action);
+  }
+  const handleToggleTodo = (id) => {
+    dispatchTodo({
+      type: '[TODO] Toggle Todo',
+      payload: id
+    });
+  }
+  const handleDeleteTodo = (id) =>{
+    console.log(id);
+    dispatchTodo({
+      type: '[TODO] Remove Todo',
+      payload: id
+    });
+  }
+  
+  return{
+    todos,
+    todosCount: todos.length, 
+    pendingTodosCount: todos.filter(todo=>!todo.done).length,
+    handleNewTodo,
+    handleToggleTodo,
+    handleDeleteTodo,
+  };
+}
